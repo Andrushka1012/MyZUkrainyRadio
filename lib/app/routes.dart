af4b6d06c6ab7ddc40b6331250f4c53340bf61cc
andriimakarenko <andrii.makarenko@euvic.pl>
2022-04-01 13:56:55 +0200

    setup navigation

---
+++ lib/app/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:myzukrainy/presentation/main_page/main_page.dart';
+
+final routes = <String, WidgetBuilder>{
+  MainPage.routeName: (_) => MainPage(),
+
+};
+
+Widget getGenerateRoutePage(RouteSettings settings) {
+  switch (settings.name) {
+  /* case EmailConfirmationPage.ROUTE_NAME:
+      return EmailConfirmationPage(settings.arguments! as EmailConfirmationPageArgs);*/
+    default:
+      throw Exception('Not screen specified to route ${settings.name}');
+  }
+}
---
+++ lib/presentation/main_page/main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  static const routeName = 'MainPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
