f398134679e478bb93a64ff2a11572861f79a199
andriimakarenko <andrii.makarenko@euvic.pl>
2022-04-02 20:41:01 +0200

    added cover disc animation

--- lib/core/presentation/widgets/mini_player/cover_disc.dart
+++ lib/core/presentation/widgets/mini_player/cover_disc.dart
@@ -1,79 +1,115 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:myzukrainy/core/domain/player/player_controller.dart';
 import 'package:myzukrainy/core/presentation/styles/colors.dart';
 import 'package:myzukrainy/core/presentation/widgets/measure_size.dart';
+import 'package:koin_flutter/koin_flutter.dart';
 
 class CoverDisc extends StatefulWidget {
   @override
   State<CoverDisc> createState() => _CoverDiscState();
 }
 
-class _CoverDiscState extends State<CoverDisc> {
+class _CoverDiscState extends State<CoverDisc> with SingleTickerProviderStateMixin {
   Size? size;
+
+  late AnimationController _animationController;
+  late StreamSubscription playbackSubscription;
+  late final PlayerController _playerController = get();
 
   double get radius => size?.width ?? 0;
 
   @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 5000),
+      vsync: this,
+    );
+    _animationController.repeat();
+
+    playbackSubscription = _playerController.playbackStateStream.listen((state) {
+      if (state.playing) {
+        _animationController.repeat();
+      } else {
+        _animationController.stop();
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: MeasureSize(
-        onChange: (newSize) {
-          setState(() {
-            size = newSize;
-          });
-        },
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(150.0),
-                child: Image(
-                  image: AssetImage('assets/images/cover_white.jpg'),
+    return RotationTransition(
+      turns: Tween(begin: 0.0, end: 1.0).animate(_animationController),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: MeasureSize(
+          onChange: (newSize) {
+            setState(() {
+              size = newSize;
+            });
+          },
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(150.0),
+                  child: Image(
+                    image: AssetImage('assets/images/cover_white.jpg'),
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: _CircleItem(
-                radius: radius / 6.9,
-                color: AppColors.mainPageHeaderColor,
+              Align(
+                alignment: Alignment.center,
+                child: _CircleItem(
+                  radius: radius / 6.9,
+                  color: AppColors.mainPageHeaderColor,
+                ),
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: _CircleItem(
-                radius: radius / 5.3,
-                color: AppColors.mainPageHeaderColorAlpha2,
+              Align(
+                alignment: Alignment.center,
+                child: _CircleItem(
+                  radius: radius / 5.3,
+                  color: AppColors.mainPageHeaderColorAlpha2,
+                ),
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: _CircleBorderItem(
-                radius: radius / 4.6,
-                color: AppColors.mainPageHeaderColorAlpha1,
-                width: 4,
+              Align(
+                alignment: Alignment.center,
+                child: _CircleBorderItem(
+                  radius: radius / 4.6,
+                  color: AppColors.mainPageHeaderColorAlpha1,
+                  width: 4,
+                ),
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: _CircleBorderItem(
-                radius: radius / 2.7,
-                color: AppColors.mainPageHeaderColorAlpha2,
-                width: radius / 13.4,
+              Align(
+                alignment: Alignment.center,
+                child: _CircleBorderItem(
+                  radius: radius / 2.7,
+                  color: AppColors.mainPageHeaderColorAlpha2,
+                  width: radius / 13.4,
+                ),
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: _CircleBorderItem(
-                radius: radius / 1.06,
-                color: AppColors.mainPageHeaderColorAlpha1,
-                width: radius / 70,
+              Align(
+                alignment: Alignment.center,
+                child: _CircleBorderItem(
+                  radius: radius / 1.06,
+                  color: AppColors.mainPageHeaderColorAlpha1,
+                  width: radius / 70,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    playbackSubscription.cancel();
+    super.dispose();
   }
 }
 
