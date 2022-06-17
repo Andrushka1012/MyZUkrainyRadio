c0e0922dc84a32271f09eeed803266effdb1a0bd
andriimakarenko <andrii.makarenko@euvic.pl>
2022-06-17 09:51:08 +0200

    code format

--- lib/helpers/app_connectivity.dart
+++ lib/helpers/app_connectivity.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:http/http.dart' as http;
-import 'package:http/io_client.dart';
 import 'package:myzukrainy/app/config/app_config.dart';
 import 'package:myzukrainy/core/domain/player/player_controller.dart';
+
+import 'closeable_multipart_request.dart';
 
 class AppConnectivity {
   final _connectivity = Connectivity();
@@ -45,39 +45,4 @@ class AppConnectivity {
   }
 
   void disposeStream() => _controller.close();
-}
-
-class CloseableMultipartRequest extends http.MultipartRequest {
-  IOClient client = IOClient(HttpClient());
-
-  CloseableMultipartRequest(String method, Uri uri) : super(method, uri);
-
-  void close() => client.close();
-
-  @override
-  Future<http.StreamedResponse> send() async {
-    try {
-      var response = await client.send(this);
-      var stream = onDone(response.stream, client.close);
-      return http.StreamedResponse(
-        http.ByteStream(stream),
-        response.statusCode,
-        contentLength: response.contentLength,
-        request: response.request,
-        headers: response.headers,
-        isRedirect: response.isRedirect,
-        persistentConnection: response.persistentConnection,
-        reasonPhrase: response.reasonPhrase,
-      );
-    } catch (_) {
-      client.close();
-      rethrow;
-    }
-  }
-
-  Stream<List<int>> onDone<T>(Stream<T> stream, void Function() onDone) =>
-      stream.transform(StreamTransformer.fromHandlers(handleDone: (sink) {
-        sink.close();
-        onDone();
-      }));
 }
---
+++ lib/helpers/closeable_multipart_request.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+
+class CloseableMultipartRequest extends http.MultipartRequest {
+  IOClient client = IOClient(HttpClient());
+
+  CloseableMultipartRequest(String method, Uri uri) : super(method, uri);
+
+  void close() => client.close();
+
+  @override
+  Future<http.StreamedResponse> send() async {
+    try {
+      var response = await client.send(this);
+      var stream = onDone(response.stream, client.close);
+      return http.StreamedResponse(
+        http.ByteStream(stream),
+        response.statusCode,
+        contentLength: response.contentLength,
+        request: response.request,
+        headers: response.headers,
+        isRedirect: response.isRedirect,
+        persistentConnection: response.persistentConnection,
+        reasonPhrase: response.reasonPhrase,
+      );
+    } catch (_) {
+      client.close();
+      rethrow;
+    }
+  }
+
+  Stream<List<int>> onDone<T>(Stream<T> stream, void Function() onDone) =>
+      stream.transform(StreamTransformer.fromHandlers(handleDone: (sink) {
+        sink.close();
+        onDone();
+      }));
+}
