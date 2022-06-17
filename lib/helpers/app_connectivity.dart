fcd3e6efcac5e4e16618f4d319d6937a8b57db46
andriimakarenko <andrii.makarenko@euvic.pl>
2022-06-17 09:49:46 +0200

    corrected issue with duplicated clients

--- lib/helpers/app_connectivity.dart
+++ lib/helpers/app_connectivity.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 import 'package:myzukrainy/app/config/app_config.dart';
 import 'package:myzukrainy/core/domain/player/player_controller.dart';
 
@@ -33,8 +34,9 @@ class AppConnectivity {
 
   Future<ControllerResult> checkIsLive() async {
     try {
-      var request = http.MultipartRequest("GET", Uri.parse(AppConfig.value.uaStreamUrl));
+      var request = CloseableMultipartRequest("GET", Uri.parse(AppConfig.value.uaStreamUrl));
       final streamedResponse = await request.send();
+      request.close();
 
       return streamedResponse.statusCode < 400 ? ControllerResult.live : ControllerResult.offline;
     } catch (_) {
@@ -43,4 +45,39 @@ class AppConnectivity {
   }
 
   void disposeStream() => _controller.close();
+}
+
+class CloseableMultipartRequest extends http.MultipartRequest {
+  IOClient client = IOClient(HttpClient());
+
+  CloseableMultipartRequest(String method, Uri uri) : super(method, uri);
+
+  void close() => client.close();
+
+  @override
+  Future<http.StreamedResponse> send() async {
+    try {
+      var response = await client.send(this);
+      var stream = onDone(response.stream, client.close);
+      return http.StreamedResponse(
+        http.ByteStream(stream),
+        response.statusCode,
+        contentLength: response.contentLength,
+        request: response.request,
+        headers: response.headers,
+        isRedirect: response.isRedirect,
+        persistentConnection: response.persistentConnection,
+        reasonPhrase: response.reasonPhrase,
+      );
+    } catch (_) {
+      client.close();
+      rethrow;
+    }
+  }
+
+  Stream<List<int>> onDone<T>(Stream<T> stream, void Function() onDone) =>
+      stream.transform(StreamTransformer.fromHandlers(handleDone: (sink) {
+        sink.close();
+        onDone();
+      }));
 }
